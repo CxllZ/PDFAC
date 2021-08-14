33cf81eca008dea00700cd5727f947abf7f50e21
CxllZ <72345758+CxllZ@users.noreply.github.com>
2021-08-14 20:54:23 +0100

    added left/right click feature + keybinds

--- autoclicker.py
+++ autoclicker.py
@@ -1,5 +1,6 @@
 import threading, pyautogui, tkinter, time
 from tkinter import messagebox
+from pynput.keyboard import Key, Listener
 
 threads = []
 thread_count = 10
@@ -10,12 +11,6 @@ pyautogui.FAILSAFE = True
 def killThreads():
     global stop_threads
     stop_threads = True
-
-def clicker():
-    while True:
-        pyautogui.click()
-        if stop_threads:
-            break
     
 def createThreads(thread_count, threads):
     for i in range(thread_count):
@@ -33,6 +28,11 @@ def startThreads(thread_count, threads):
 
 def mainProgram():
     try:
+        global timing_delay
+        timing_delay = int(time_entry.get())
+    except ValueError:
+        messagebox.showerror("Time Error", "Enter Only Integer Not String!")
+    try:
         global threads
         global thread_count
         thread_count = int(threads_entry.get())
@@ -42,42 +42,91 @@ def mainProgram():
             resetThreads(thread_count, threads)
     except ValueError:
         messagebox.showerror("CPS Error", "Enter Only Integer Not string!")
-    try:
-        time.sleep(int(time_entry.get()))
-    except ValueError:
-        messagebox.showerror("Time Error", "Enter Only Integer Not string!")
     startThreads(thread_count, threads)
 
+def clicker():
+    while True:
+        selection = click_btn.get()
+        if selection == 1:
+            item = "Left Click On"
+            pyautogui.leftClick()
+        elif selection == 2:
+            item = "Right Click On"
+            pyautogui.rightClick()
+
+        label.config(text=item)
+        time.sleep(timing_delay)
+        if stop_threads:
+            break
+
+def on_press(key):
+    pass
+
+def on_release(key):
+    if key == Key.esc:
+        killThreads()
+        root.quit()
+        print("Exiting")
+        return False
+    elif key == Key.f1:
+        mainProgram()
+        print("Started")
+    elif key == Key.f2:
+        resetThreads(thread_count, threads)
+        killThreads()
+        print("Paused")
+    else:
+        pass
+
+def keylistener():
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+def exit():
+    killThreads()
+    root.quit()
+    print("Exiting")
+    return False
+
 root = tkinter.Tk()
-root.geometry("300x160")
+root.title("PDFAC - Pretty Damn Fast Autoclicker")
+root.geometry("400x250")
 root.resizable(False, False)
 
-w = tkinter.Label(root, text="Enter time in seconds to wait before clicking")
+click_btn = tkinter.IntVar()
+
+LeftClickEntry = tkinter.Radiobutton(root, text="Left Click", padx = 20, variable=click_btn,  value=1)
+LeftClickEntry.pack()
+
+RightClickEntry = tkinter.Radiobutton(root, text="Right Click", padx = 30, variable=click_btn, value=2)
+RightClickEntry.pack()
+
+label = tkinter.Label(root)
+label.pack()
+
+w = tkinter.Label(root, text="Enter delay in seconds (0 for fast clicks, above 3 for afk clicks)")
 w.pack()
 
 time_entry = tkinter.Entry(root)
 time_entry.pack()
 
-t = tkinter.Label(root, text="Enter CPS (30 may get serious)")
+t = tkinter.Label(root, text="Enter CPS (5 recommended for fast clicks)")
 t.pack()
 
 threads_entry = tkinter.Entry(root)
 threads_entry.pack()
 
-start_button = tkinter.Button(root, text="START?", command=mainProgram)
+start_button = tkinter.Button(root, text="START     (F1)", command=mainProgram)
 start_button.pack()
 
-def stop():
-    killThreads()
-    
-def exit():
-    root.destroy()
-
-stop_button = tkinter.Button(root, text="STOP?)", command=stop, padx=1)
+stop_button = tkinter.Button(root, text="PAUSE      (F2)", command=killThreads, padx=1)
 stop_button.pack()
 
-exit_button = tkinter.Button(root, text="EXIT?)", command=exit, padx=2)
+exit_button = tkinter.Button(root, text="EXIT       (ESC)", command=exit, padx=2)
 exit_button.pack()
 
+tg = threading.Thread(target=keylistener)
+tg.start()
 root.wm_attributes("-topmost", 1)
+root.protocol("WM_DELETE_WINDOW", exit)
 root.mainloop()
